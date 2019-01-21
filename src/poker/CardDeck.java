1a83ee43e14c95a870599fb1c62a0cfefde8c5f1
moebear <longk85@gmail.com>
2019-01-21 16:10:44 +0800

    commit src/ file

--- src/poker/CardDeck.java
+++ src/poker/CardDeck.java
@@ -1,23 +1,30 @@
 package poker;
 
+/**
+ * @author Moebear
+ * 这个类用来表示一副扑克牌。
+ */
 public class CardDeck {
-	private int cardsNum = 52;
-	private String[] suits = {"Heart", "Spade", "Dimond", "Club"};
-	private String[] ranks = {"A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"};
-	private int[] cardsID = new int[cardsNum];
-	private String[][] cardsName = new String[suits.length][ranks.length];
+	private int cardsNum = 52; //一副牌的牌数
+	private String[] suits = {"Heart", "Spade", "Dimond", "Club"}; //四种花色的名字
+	private String[] ranks = {"A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"}; //十三个牌值
+	private int[] cardsID = new int[cardsNum]; //每张牌的ID序列
+	private String[][] cardsName = new String[suits.length][ranks.length]; //每张牌的名字，包括花色和牌值
 	
+	/**
+	 * The constructing method of class CardDeck.
+	 */
 	public CardDeck() {
 		for(int i=0; i<cardsNum; i++) {
 			cardsID[i] = i;
 		}
-		for(int i=0; i<suits.length; i++) {
-			for(int j=0; j<ranks.length; j++) {
-				cardsName[i][j] = suits[i] + " " + ranks[j];
-			}
-		}
 	}
 	
+	/**
+	 * 通过牌的ID来获取牌的名字
+	 * @param cardID 牌的ID
+	 * @return 牌的名字
+	 */
 	public String getCardName(int cardID) {
 		int suitsID;
 		int ranksID;
--- src/poker/VideoPoker.java
+++ src/poker/VideoPoker.java
@@ -2,17 +2,24 @@ package poker;
 
 import java.util.*;
 
+/**
+ * @author moebear
+ * Jacks or Better 的基本规则和游戏逻辑
+ */
 public class VideoPoker {
-	static private int handCardsNum = 5;
-	private CardDeck newDeck = new CardDeck();
-	private int[] handCards = new int[handCardsNum];
-	private String[] rankList = {"Royal flush", "Straight flush", "4 of a kind", "Full house", "Flush", "Straight", "3 of a kind", "Two pair", "Jacks or better"};
-	private int[] reward = {4000, 250, 125, 45, 30, 20, 15, 10, 5};
+	private int handCardsNum = 5; //手牌数量
+	private CardDeck newDeck = new CardDeck(); //每一局游戏创建一副新牌组
+	private int[] handCards = new int[handCardsNum]; //手牌的ID序列
+	private String[] rankList = {"Royal flush", "Straight flush", "4 of a kind", "Full house", "Flush", "Straight", "3 of a kind", "Two pair", "Jacks or better"}; //中奖牌型的名字序列
+	private int[] reward = {4000, 250, 125, 45, 30, 20, 15, 10, 5}; //对应的中奖牌型的奖励回报
 	
 	public VideoPoker() {
 		
 	}
 	
+	/**
+	 * 随机生成五张手牌
+	 */
 	public void generateHandCards() {
 		Random random = new Random();
 		int end = newDeck.getCardsNum() - 1;
@@ -25,6 +32,10 @@ public class VideoPoker {
 		}	
 	}
 	
+	/**
+	 * 在玩家决定舍弃的牌后随机生成新牌
+	 * @param changeFlag 用来表示舍弃哪些牌：true 舍弃； false 保留
+	 */
 	public void changeHandCards(boolean[] changeFlag) {
 		Random random = new Random();
 		int end = newDeck.getCardsNum() - 6;
@@ -39,17 +50,29 @@ public class VideoPoker {
 		}
 	}
 	
+	/**
+	 * 中奖牌型检查
+	 * @param handCards 待检查的手牌ID序列
+	 * @return 中奖牌型的名字
+	 */
 	public String checkRank(int[] handCards) {
 		String rank = "";
-		int[] suitList = new int[handCards.length];
-		int[] rankList = new int[handCards.length];
+		int[] suitList = new int[handCards.length]; //花色ID序列
+		int[] rankList = new int[handCards.length]; //牌值ID序列
+		boolean flush;
+		boolean straight;
+		int pairs;
+		int trikind;
+		int forkind;
 		
+		/* 获取手牌的花色ID序列 */
 		for(int i=0; i<handCards.length; i++) {
 			int num;
 			num = handCards[i] / 13;
 			suitList[i] = num;
 		}
 		
+		/* 获取手牌的牌值ID序列 */
 		for(int i=0; i<handCards.length; i++) {
 			int num;
 			num = handCards[i] % 13;
@@ -59,6 +82,11 @@ public class VideoPoker {
 		return rank;
 	}
 	
+	/**
+	 * 检查牌型是否是同花
+	 * @param suitList 手牌花色ID序列
+	 * @return 是或否
+	 */
 	public boolean checkFlush(int[] suitList) {
 		HashSet<Integer> suitCheck = new HashSet<Integer>();
 		boolean result = false;
@@ -75,6 +103,11 @@ public class VideoPoker {
 		return result;
 	}
 	
+	/**
+	 * 检查是否顺子
+	 * @param rankList 手牌牌值ID序列
+	 * @return 是或否
+	 */
 	public boolean checkStraight(int[] rankList) {
 		List<Integer> straightCheck = new ArrayList<Integer>();
 		boolean result = false;
@@ -83,14 +116,17 @@ public class VideoPoker {
 			straightCheck.add(rankList[i]);
 		}
 		
+		/* 将牌值序列按递增顺序重排 */
 		Collections.sort(straightCheck);
 		
+		/* 检查首尾牌值差值，如果不等于4且不等于12则不是顺子（等于12是A、2、3、4、5的情况） */
 		int len = straightCheck.size();
 		int temp = Math.abs(straightCheck.get(0) - straightCheck.get(len - 1));
-		if(temp > 4 && temp != 12) {
+		if(temp != 4 && temp != 12) {
 			return result;
 		}
 		
+		/* 检查是否有相同的牌 */
 		for(int i=0; i<len-1; i++) {
 			temp = straightCheck.get(i+1) - straightCheck.get(i);
 			if(temp == 0) {
@@ -101,6 +137,12 @@ public class VideoPoker {
 		result = true;
 		
 		return result;
+	}
+	
+	public String checkSameCards(int[] rankList) {
+		int pairs;
+		int trikind;
+		int forkind;
 	}
 
 }
